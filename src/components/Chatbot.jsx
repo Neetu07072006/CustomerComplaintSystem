ce702ad6e885210372f38094e46e6cf085aae767
Neetu07072006 <n48891271@gmail.com>
2026-09-04 09:13:26 +0530

    Rename Chatbot CSS to fix case sensitivity bug

--- src/components/Chatbot.jsx
+++ src/components/Chatbot.jsx
@@ -1,6 +1,6 @@
 import { useState, useEffect, useRef } from "react";
 import { useNavigate } from "react-router-dom";
-import "./Chatbot.css";
+import "./ChatbotStyles.css";
 
 function Chatbot() {
   const navigate = useNavigate();
